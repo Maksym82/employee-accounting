c0524ff4d9ecc2f7e183f3c5ff95552e945f37d8
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-05-12 20:17:48 +0300

    Update app

--- employees/src/components/app/app.js
+++ employees/src/components/app/app.js
@@ -1,3 +1,4 @@
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -7,19 +8,26 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
-  return (
-    <div className="app">
-        <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
+    const data = [
+        {name: 'John C.', salary: 800, increase: false},
+        {name: 'Alex M.', salary: 3000, increase: true},
+        {name: 'Carl W.', salary: 5000, increase: false}
+    ];
+
+    return (
+        <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+            
+            <EmployeesList data={data}/>
+            <EmployeesAddForm/>
         </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+    );
 }
 
 export default App;
