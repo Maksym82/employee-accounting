f796ba471d8170e4bb2f86537ebdbe86e3cf1de9
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-05-14 12:11:20 +0300

    Update App

--- employees/src/components/app/app.js
+++ employees/src/components/app/app.js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
@@ -7,27 +8,35 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
-
-    const data = [
-        {name: 'John C.', salary: 800, increase: false, id: 1},
-        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
-        {name: 'Carl W.', salary: 5000, increase: false, id: 3}
-    ];    
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'John C.', salary: 800, increase: false, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+            ]  
+        }
+    }
     
-    return (
-        <div className="app">
-            <AppInfo />
-
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    render() {
+        return (
+            <div className="app">
+                <AppInfo />
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                
+                <EmployeesList 
+                    data={this.state.data}
+                    onDelete={id => console.log(id)}/>
+                <EmployeesAddForm/>
             </div>
-            
-            <EmployeesList data={data}/>
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
